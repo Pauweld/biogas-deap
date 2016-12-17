681db903ea826beb57dd522d44d54f657a2c5c83
Pauwel <pauwel.d-almeida-ayivi@etu.univ-nantes.fr>
2016-12-17 10:08:01 +0100

    imp : pred.py

--- pred.py
+++ pred.py
@@ -1,7 +1,48 @@
 from deap import *
+import argparse
 
-def main():
-    print('coucou')
+def main(a,b,gen):
+    #data
+    fa = open(a,'r')
+    header = fa.readline()
+    nb_intrants_d = len(header.split(';'))-2
+    name_intrants_d = header.split(';')[2:]
+    fa.close()
+    
+    #cinetics
+    fb = open(b,'r')
+    header = fb.readline()
+    nb_intrants_c = len(header.split(';'))-1
+    name_intrants_c = header.split(';')[1:]
+    fb.close()
+
+    #check compatibility
+    if nb_intrants_c != nb_intrants_d:
+        print('Fichiers érronés')
+        return
+    for i in name_intrants_c:
+        if i not in name_intrants_d:
+            print('Fichiers érronés')
+            return
+
+    #algorithm
+    for j in range(0,gen):
+        for k in range(0,nb_intrants_c):
+            print('coucou')   
 
 if __name__ == "__main__":
-    main()
+    #arguments
+    parser = argparse.ArgumentParser(
+    description="Get stats from Powertool output")
+    parser.add_argument('-d', '--donnees', type=str, default='donnees.csv',
+                        required = True,
+                        help="Data in .csv format")
+    parser.add_argument('-c', '--cinetiques', type=str, default='cinetiques.csv',
+                        required = True,
+                        help="Cinetic curves in .csv format")
+    parser.add_argument('-g', '--generations', type=int, default=100, required=True,
+                        help="Number of generations to make")
+    args = parser.parse_args()
+    
+    #start 
+    main(args.donnees,args.cinetiques,args.generations)

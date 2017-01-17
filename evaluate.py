800e0a86643ae98944465933abedf0fa0ac31668
Pauwel <pauwel.d-almeida-ayivi@etu.univ-nantes.fr>
2017-01-17 16:06:20 +0100

    correction evalute

--- evaluate.py
+++ evaluate.py
@@ -10,6 +10,7 @@ def Fitness(individual,f,num_intrant):
     r = random.randint(60,120)
     reelle = data[num_intrant-1][r]
     somme = 0
-    for i,j in zip(range(r-60,r),range(1,60)):
+    for i,j in zip(range(r-60,r),range(1,individual[0][-2])):
+        #print('i',i,'j',j,'ind',individual)
         somme += getValue(individual,j) * int(data[num_intrant][i])
     return  somme,somme/float(reelle)

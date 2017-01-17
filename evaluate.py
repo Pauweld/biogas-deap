89fdba5a2e463900129e2ebe94bf5e0b9f94eefd
Pauwel <pauwel.d-almeida-ayivi@etu.univ-nantes.fr>
2017-01-17 16:15:15 +0100

    fixed evaluate probleme

--- evaluate.py
+++ evaluate.py
@@ -7,10 +7,9 @@ individual = [[5,10.1,10,20.2,20,20.5,25,10.6,40,7,50,5,60,0]]
 
 def Fitness(individual,f,num_intrant):
     data = fill_data(f)
-    r = random.randint(60,120)
+    r = random.randint(61,121)
     reelle = data[num_intrant-1][r]
     somme = 0
-    for i,j in zip(range(r-60,r),range(1,individual[0][-2])):
-        #print('i',i,'j',j,'ind',individual)
+    for i,j in zip(range(r-59,r),range(1,individual[0][-2])):
         somme += getValue(individual,j) * int(data[num_intrant][i])
     return  somme,somme/float(reelle)

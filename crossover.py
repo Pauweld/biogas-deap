8acf84353a09e43a83da75ec5fac9f1880d368a3
Arnaud <arnaud.m0527@gmail.com>
2017-01-26 16:21:36 +0100

    oubli de retirer main dans croisement

--- crossover.py
+++ crossover.py
@@ -1,7 +1,7 @@
 import random
 
-ind1 = [[5,10.1,10,20.2,20,20.2,25,10.6,40,7,50,5,60,0]]
-ind2 = [[4,5.1,6,18.2,25,18.2,30,12,40,10,50,7,55,0]]
+#ind1 = [[5,10.1,10,20.2,20,20.2,25,10.6,40,7,50,5,60,0]]
+#ind2 = [[4,5.1,6,18.2,25,18.2,30,12,40,10,50,7,55,0]]
 
 def crossover(ind1, ind2):
     size = len(ind1[0])
@@ -13,5 +13,5 @@ def crossover(ind1, ind2):
     
     return ind1, ind2
 
-if __name__ == "__main__":
-    print(crossover(ind1,ind2))
+#if __name__ == "__main__":
+    #print(crossover(ind1,ind2))

eec987ac881085c0dcb1496ff74387dc60a460d9
Arnaud <arnaud.m0527@gmail.com>
2017-01-27 16:40:36 +0100

    crossover is ok

--- crossover.py
+++ crossover.py
@@ -4,21 +4,20 @@ import random
 #ind2 = [[4,5.1,6,18.2,25,18.2,30,12,40,10,50,7,55,0]]
 
 def crossover(ind1, ind2):
-    tmp = 0
     size = len(ind1[0])
     cxpoint = random.randint(1, size - 1)
     while((cxpoint % 2) != 0 or cxpoint == 2 or cxpoint == 4):
         cxpoint = random.randint(1, size - 1)
     ind1[0][cxpoint:], ind2[0][cxpoint:] = ind2[0][cxpoint:], ind1[0][cxpoint:]
-    tmp = cxpoint
-    while(tmp != 12):
-        print(tmp)
-        if(ind1[0][tmp+2] < ind1[0][tmp]):
-            ind1[0][tmp+2] = ind1[0][tmp]
-        if(ind2[0][tmp+2] < ind2[0][tmp]):
-            ind2[0][tmp+2] = ind2[0][tmp]
-        tmp = tmp+2
-    
+    for i in range(cxpoint,len(ind1[0])):
+        if(i%2 == 0):
+            if(ind1[0][i-2] > ind1[0][i]):
+                ind1[0][i] = ind1[0][i-2]
+    for i in range(cxpoint,len(ind2[0])):
+        if(i%2 == 0):
+            if(ind2[0][i-2] > ind2[0][i]):
+                ind2[0][i] = ind2[0][i-2]
+                
     return ind1, ind2
 
 #if __name__ == "__main__":

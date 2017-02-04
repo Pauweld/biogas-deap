ba04fead81fa6fd020787b48ab7055d0e42e4843
Arnaud <arnaud.m0527@gmail.com>
2017-02-04 10:20:00 +0100

    petit modif mutation

--- mutation.py
+++ mutation.py
@@ -1,9 +1,8 @@
 # ce fichier implémente la mutation pour un individu donné
 
 import random
-#from plot import *
 
-#ind = [[5,10.1,10,20.2,20,20.2,25,10.6,40,7,50,5,60,0]]
+ind = [[5,10.1,10,20.2,15,20.2,19,10.6,20,7,30,5,35,0]]
 
 def mutation(individual):
     i = random.randint(0,len(individual[0])-1)
@@ -15,6 +14,7 @@ def mutation(individual):
         individual[0][1] = round(individual[0][1], 2)
     elif(i == 12):
         individual[0][12] = random.randint(individual[0][10],individual[0][10]*1.5)
+        individual[0][12] = round(individual[0][12], 2)
     elif(i == 2):
         individual[0][2] = random.randint(individual[0][0],individual[0][4])
         individual[0][3] = random.uniform(individual[0][1],individual[0][3])
@@ -32,6 +32,6 @@ def mutation(individual):
 
     return individual
 
-#if __name__ == "__main__":
+if __name__ == "__main__":
 
-    #show_plot(mutation(ind),'a')
+    print(mutation(ind))
